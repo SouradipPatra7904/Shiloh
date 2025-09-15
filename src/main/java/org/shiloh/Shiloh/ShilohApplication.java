32c7ed3858a38b215116707ca0f23f693f6716c7
Souradip Patra <spectralphoenix.dev@gmail.com>
2025-09-15 23:08:58 +0530

    New Levels, New Devils : Scaffolding complete. Enhancements remain.

---
+++ src/main/java/org/shiloh/Shiloh/ShilohApplication.java
@@ -0,0 +1,13 @@
+package org.shiloh.Shiloh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShilohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShilohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shiloh/Shiloh/ShilohApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shiloh.Shiloh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShilohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
